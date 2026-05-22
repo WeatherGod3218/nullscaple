0d63f027f1f5b1bb705afef65016a21c16103d9b
WeatherGod3218 <weathergod3218@gmail.com>
2026-05-21 20:19:24 -0500

    feat: Front end communicates with API

--- enemies/enemies.go
+++ enemies/enemies.go
@@ -18,28 +18,31 @@ const (
 	Lower  Comparison = "lower"
 )
 
-var ValidModes = map[string]bool{
-	"casual":   true,
-	"standard": true,
-	"extreme":  true,
-}
-
+// Internal Enemy Data
 type EnemyData struct {
 	Id          int    `json:"id"`
 	Name        string `json:"name"`
 	CurseAmount int    `json:"curse_amount"`
 	StartLevel  int    `json:"start_level"`
+	KillMethod  string `json:"kill_method"`
+	Filename    string `json:"filename"`
 }
 
+// Internal Enemy Data
 type EnemyComparison struct {
 	Id          bool       `json:"id"`
 	Name        bool       `json:"name"`
+	KillMethod  bool       `json:"kill_method"`
 	CurseAmount Comparison `json:"curse_amount"`
 	StartLevel  Comparison `json:"start_level"`
 }
 
+type EnemyRequest struct {
+	ID   string `json:"enemy_id"`
+	Mode string `json:"gameplay_mode"`
+}
+
 var LoadedEnemies map[int]*EnemyData
-var EnemyList map[int]string
 
 func hashString(s string) uint32 {
 	h := fnv.New32a()
@@ -57,6 +60,7 @@ func compareInts(selected int, base int) Comparison {
 	return Equal
 }
 
+// Initializes enemy data from the enemies.json file in the root directory. Establishes loadedServerEnemies and loadedClientEnemies as their respective types
 func InitEnemies() {
 	logging.Logger.WithFields(logrus.Fields{"module": "enemies", "method": "InitEnemies"}).Info("Starting enemy loading!")
 
@@ -72,19 +76,17 @@ func InitEnemies() {
 		logging.Logger.WithFields(logrus.Fields{"error": err, "module": "enemies", "method": "InitEnemies"}).Fatal("failed to unmarshal the json!")
 	}
 
-	LoadedEnemies = make(map[int]*EnemyData)
-	EnemyList = make(map[int]string)
+	LoadedEnemies = make(map[int]*EnemyData, len(enemies))
 
 	for _, enemy := range enemies {
 		LoadedEnemies[enemy.Id] = &enemy
-		EnemyList[enemy.Id] = enemy.Name
 	}
 
 	logging.Logger.WithFields(logrus.Fields{"module": "enemies", "method": "InitEnemies"}).Info("Succesfully loaded all enemies!")
 }
 
 func GetEnemyOfTheDay(mode string) *EnemyData {
-	if len(EnemyList) == 0 {
+	if len(LoadedEnemies) == 0 {
 		logging.Logger.WithFields(logrus.Fields{"module": "enemies", "method": "GetEnemyOfTheDay"}).Warn("enemy list is empty, returning fake enemy!")
 		return nil
 	}
@@ -112,24 +114,28 @@ func GetEnemyFromId(id int) *EnemyData {
 	return nil
 }
 
-func CompareEnemies(selectedEnemy EnemyData, baseEnemy EnemyData) EnemyComparison {
+func CompareEnemies(selectedEnemy *EnemyData, baseEnemy *EnemyData) EnemyComparison {
 	return EnemyComparison{
 		Id:          selectedEnemy.Id == baseEnemy.Id,
 		Name:        selectedEnemy.Name == baseEnemy.Name,
+		KillMethod:  selectedEnemy.KillMethod == baseEnemy.KillMethod,
 		CurseAmount: compareInts(selectedEnemy.CurseAmount, baseEnemy.CurseAmount),
 		StartLevel:  compareInts(selectedEnemy.StartLevel, baseEnemy.StartLevel),
 	}
 }
 
-func GetEnemyList() map[int]string {
-	if EnemyList == nil {
+func GetEnemyList() map[int]*EnemyData {
+	if LoadedEnemies == nil {
 		logging.Logger.WithFields(logrus.Fields{"module": "enemies", "method": "GetEnemyList"}).Warn("enemy list is empty, returning fake enemy!")
-		return make(map[int]string)
+		return nil
 	}
-	return EnemyList
+	return LoadedEnemies
 }
 
 func CheckIfStringIsMode(mode string) bool {
-	_, exists := ValidModes[mode]
-	return exists
+	switch mode {
+	case "casual", "standard", "extreme":
+		return true
+	}
+	return false
 }
